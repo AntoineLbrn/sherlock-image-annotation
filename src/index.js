2186ac714bcd5f449650b3cba05fcb27f6337454
Antoine Lebrun <antoine.lebrun@etu.utc.fr>
2021-09-30 22:39:30 +0200

    set up mirador

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import mirador from 'mirador/dist/es/src/index';
+//import annotationPlugins from './mirador-annotations/index';
+//import SherlockAdapter from "./SherlockAdapter";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const config = {
+  id: 'root',
+  /*annotation: {
+    adapter: (canvasId) => new SherlockAdapter(canvasId, "http://localhost:3030/sherlock-concept/sparql", "http://localhost:5555"),
+    exportLocalStorageAnnotations: true, // display annotation JSON export button,
+  },*/
+  windows: [{
+    imageToolsEnabled: true,
+    imageToolsOpen: true,
+    manifestId: 'https://purl.stanford.edu/sn904cj3429/iiif/manifest',
+  }],
+  window: {
+    //defaultSideBarPanel: 'annotations',
+    sideBarOpenByDefault: true
+  },
+  theme: {
+    palette: {
+      primary: {
+        main: '#1967d2',
+      },
+    },
+  },
+};
+
+mirador.viewer(config, [
+  //annotationPlugins
+]);
